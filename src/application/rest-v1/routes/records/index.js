18d1100acbbb659aa61a0e38554773d3d5096c61
fidopaca <fidopaca@gmail.com>
2022-02-13 22:42:00 +0300

    Add http method not found route for records

--- src/application/rest-v1/routes/records/index.js
+++ src/application/rest-v1/routes/records/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const MethodNotAllowed = require("../../../../lib/errors/MethodNotAllowed");
 const record = require("../../middlewares/record");
 
 router.route("/").post(record.getRecords);
+
+router.use("*", (req, res, next) => {
+  next(new MethodNotAllowed(`${req.method} method is not allowed.`));
+});
 
 module.exports = router;
